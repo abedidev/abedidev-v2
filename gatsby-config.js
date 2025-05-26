a7b2c7485490cef280da9c5b0834a2f903e8ad84
Ali <abediee@gmail.com>
2025-05-26 18:09:21 -0400

    Recent changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Ali Abedi',
+    description: 'Ali Abedi is a Machine Learning Engineer and Data Scientist.',
+    siteUrl: 'https://www.abedi.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -19,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Ali Abedi',
+        short_name: 'Ali Abedi',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
