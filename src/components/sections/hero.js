d83e1c7b58e0dc39ec88ab561ca088687357dbfe
Ali <abediee@gmail.com>
2025-05-26 16:29:32 -0400

    Update Hero section headline

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,8 +60,10 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Ali Abedi.</h2>;
+  const three = (
+    <h3 className="big-heading">I am a machine learning engineer and data scientist.</h3>
+  );
   const four = (
     <>
       <p>
